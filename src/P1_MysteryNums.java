03db8b963532de18f1aa113c7d7d3ece711737e8
Destiny Goubeaux <dgoubeaux1@live.maryville.edu>
2018-03-26 00:39:47 -0500

    changes made

--- src/P1_MysteryNums.java
+++ src/P1_MysteryNums.java
@@ -12,12 +12,25 @@
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
- 
- 
+ 15 42
+ 10 25
  */
 
 public class P1_MysteryNums {
 
-
+	 public static void main(String[] args) {
+	        int x = 15;
+	        sentence( x, 42 );
+	        
+	        int y = x - 5;
+	        sentence( y, x + y);
+	    }
+	    
+	    public static void sentence( int num1, int num2 ) {
+	        System.out.println( num1 + " " + num2 );
+	    }
 
 }
+
+/* my prediction was right 
+*/
--- src/P4_MysteryTouch.java
+++ src/P4_MysteryTouch.java
@@ -6,18 +6,41 @@
 
 /*
  4. Your output prediction:
-  
+touch your eye to your head
+touch your head to your eye
+touch your shoulders to your elbow
+touch your eyes and ears to your eyes and ears
+touch your toes to your Toes
+touch your shoulders to your knees toes
+
   
  */
 
 /*
  6. Were you correct? Explain if you were not (what did you learn?)
- 
+I was correct. 
  
  */
 
 public class P4_MysteryTouch {
-
+	public static void main(String[] args) {
+        String head = "shoulders";
+        String knees = "toes"; 
+        String elbow = "head";
+        String eye = "eyes and ears";
+        String ear = "eye"; 
+        
+        touch(ear, elbow);
+        touch(elbow, ear); 
+        touch(head, "elbow"); 
+        touch(eye, eye);
+        touch(knees, "Toes"); 
+        touch(head, "knees " + knees );
+    }
+    
+    public static void touch(String elbow, String ear) { 
+        System.out.println( "touch your " + elbow + " to your " + ear );
+    }
 
 
 }
